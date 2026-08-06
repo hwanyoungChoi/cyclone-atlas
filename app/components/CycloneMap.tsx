ba23267bf837e09ba3be38fb87fceb4510542dca
hwanyoungChoi <welcomechoi@kakao.com>
2026-08-07 01:28:39 +0900

    Bundle MapLibre worker dependencies

--- app/components/CycloneMap.tsx
+++ app/components/CycloneMap.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import * as maplibregl from "maplibre-gl";
-import maplibreWorkerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?url";
+import maplibreWorkerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url";
 import "maplibre-gl/dist/maplibre-gl.css";
 import type { Storm } from "../data/storms";
 import { displayStormName } from "../data/typhoon-names-ko";
